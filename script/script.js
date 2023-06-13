a4e4523a62fc70b2bde09ae08c0f39aaff65b3f2
TALEX1995 <tanase.alex@libero.it>
2023-06-13 15:25:48 +0200

    Create Array

--- script/script.js
+++ script/script.js
@@ -1,1 +1,13 @@
 console.log('JS OK')
+
+// Exercise 1
+
+
+// Array
+
+const guests = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin'];
+
+
+// Create an Array to insert object with guests info
+
+let infoGuests = []
