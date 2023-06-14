f2b420ac9b0793159bfb13b8b814623e1157a2f3
TALEX1995 <tanase.alex@libero.it>
2023-06-14 15:31:17 +0200

    Added Array

---
+++ script/script3.js
@@ -0,0 +1,39 @@
+console.log('JS OK')
+
+// Create Array
+
+const bike = 
+[
+    {
+        name: 'Mountain Bike',
+        weight: 30
+    }
+    {
+        name: 'Ducati',
+        weight: 32
+    }
+    {
+        name: 'Fantic',
+        weight: 28
+    }
+    {
+        name: 'Torbado',
+        weight: 37
+    }
+    {
+        name: 'Fondriest',
+        weight: 30
+    }
+    {
+        name: 'Olympia',
+        weight: 25
+    }
+    {
+        name: 'Carbon Bike',
+        weight: 23
+    }
+    {
+        name: 'Heavy bike',
+        weight: 45
+    }
+]
