7dbe05aaf02ad4415b307f397e4eb6885dcb0840
TALEX1995 <tanase.alex@libero.it>
2023-06-14 17:06:57 +0200

    Added cicle to randomize points and fouls number

--- script/script4.js
+++ script/script4.js
@@ -1,1 +1,64 @@
 console.log('JS OK')
+
+// Create Array
+
+const teamFootball = 
+[
+    {
+        nome: 'Juventus',
+        punti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: 'Milan',
+        punti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: 'Roma',
+        punti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: 'Lazio',
+        punti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: 'Fiorentina',
+        punti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: 'Inter',
+        punti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: 'Bologna',
+        punti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: 'Atalanta',
+        punti: 0,
+        falliSubiti: 0,
+    },
+]
+
+
+
+// Cicle to randomize punti and falli
+
+teamFootball.forEach((team) => {
+// Randomize points
+team.punti =  Math.floor(Math.random() * 100) + 1 
+// Randomize fouls
+team.falliSubiti = Math.floor(Math.random() * 40) + 1
+})
+
+
+
+console.log(teamFootball)
+
+
